05ca95d6ad3a80b3ab0a0c635b49297cdddda605
Hennie Scheepers <hennie.104@hotmail.com>
2023-04-11 13:43:41 +0200

    Added rent to expense object

--- script.js
+++ script.js
@@ -8,6 +8,15 @@ const size = 'large'
 const expenses = {
     food: '51.7501',
     transport:  '10.2',
+    rent: {
+        none: 0,
+        'small-room': 200,
+        'large-room': 300,
+        'small-apartment': 400,
+        'large-apartment': 800,
+        'small-house': 1200,
+        'large-house': 2400,
+    }
 }
 
 // Changed every key to string as a variable name cannot consist of numbers unless it's a string
@@ -19,15 +28,15 @@ const tax = {
     '502': '42%',
 }
 
-const rent = {
-    none: 0,
-    'small-room': 200,
-    'large-room': 300,
-    'small-apartment': 400,
-    'large-apartment': 800,
-    'small-house': 1200,
-    'large-house': 2400,
-}
+// const rent = {
+//     none: 0,
+//     'small-room': 200,
+//     'large-room': 300,
+//     'small-apartment': 400,
+//     'large-apartment': 800,
+//     'small-house': 1200,
+//     'large-house': 2400,
+// }
 
 // You can change below however you want
 
@@ -35,5 +44,5 @@ const taxAsDecimal = parseFloat(tax[913]) / 100
 // Added bracket to calculate 1 - tax
 const startingAfterTax = salary * (1 - taxAsDecimal)
 const type = `${size}-${lodging}`
-const balance = startingAfterTax - parseFloat(expenses.transport) - parseFloat(expenses.food) - rent[type]
+const balance = startingAfterTax - parseFloat(expenses.transport) - parseFloat(expenses.food) - expenses.rent[type]
 console.log(parseFloat(balance).toFixed(2))
